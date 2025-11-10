b0b71c4e4af997bcf9e117c147b35972aada935c
Ulveboss <mikkel@iploug.dk>
2025-11-10 15:08:40 +0100

    asd

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,7 +3,7 @@ const SPEED = 100.0
 
 func _physics_process(_delta: float) -> void:
 	velocity = Vector2.ZERO
-	
+	look_at(get_global_mouse_position())
 	if Input.is_action_pressed("up"):
 		velocity.y -= SPEED
 	if Input.is_action_pressed('down'):
@@ -12,6 +12,9 @@ func _physics_process(_delta: float) -> void:
 		velocity.x -= SPEED
 	if Input.is_action_pressed('right'):
 		velocity.x += SPEED
+		
+	if Input.is_action_just_pressed('shoot'):
+		print('hej')
 	
 	velocity = velocity.limit_length(SPEED)
 	move_and_slide()
