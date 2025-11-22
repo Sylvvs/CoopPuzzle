dd86c33914f25fbd029d7922dbcce6c9075f43bb
Sylvvs <kongsted.simon@gmail.com>
2025-11-22 09:16:26 +0100

    testing gameover

---
+++ scripts/game_over.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var leaderboard_container := $red_abov/Margins/ContentHolder/Leaderboard
+
+func _ready() -> void:
+	DatabaseHandler.connect("highscores_received", Callable(self, "_on_highscores_received"))
+	DatabaseHandler.get_highscores(10, 0)
+	fade_in()
+
+func fade_in():
+	var tween = create_tween()
+	tween.tween_property(self, "modulate", Color(1.0, 1.0, 1.0, 1), 0.5)
+
+
+func _on_highscores_received(scores: Array):
+
+	leaderboard_container.queue_free_children()
+
+	for score_entry in scores:
+		var name = score_entry.get("name", "Unknown")
+		var score = score_entry.get("score", 0)
+		
+		var label = Label.new()
+		label.text = "%s - %d" % [name, score]
+		leaderboard_container.add_child(label)
+		
+	print("Leaderboard updated!")
